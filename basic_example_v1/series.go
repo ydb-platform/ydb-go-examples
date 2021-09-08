6da5654f2a18e1da30b84699ba2f9babafe10d84
asmyasnikov <79263256394@ya.ru>
2021-09-08 17:36:37 +0300

    renames

--- basic_example_v1/series.go
+++ basic_example_v1/series.go
@@ -10,9 +10,9 @@ import (
 	"text/template"
 	"time"
 
+	"github.com/ydb-platform/ydb-go-examples/pkg/cli"
 	"github.com/ydb-platform/ydb-go-sdk/v3"
 	"github.com/ydb-platform/ydb-go-sdk/v3/connect"
-	"github.com/ydb-platform/ydb-go-examples/pkg/cli"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table"
 )
 
@@ -83,6 +83,7 @@ func (cmd *Command) Run(ctx context.Context, params cli.Parameters) error {
 	db, err := connect.New(
 		connectCtx,
 		params.ConnectParams,
+		connect.WithAnonymousCredentials(),
 		connect.WithSessionPoolIdleThreshold(time.Second*5),
 		connect.WithSessionPoolKeepAliveMinSize(-1),
 		connect.WithDiscoveryInterval(5*time.Second),
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/ydb-platform/ydb-go-examples
 
 go 1.16
 
-require github.com/ydb-platform/ydb-go-sdk/v3 v3.0.0-20210908142640-2c93fa8ed51f
+require (
+	github.com/ydb-platform/ydb-go-sdk/v3 v3.0.0-20210908143000-f0b730122ae9
+	golang.org/x/net v0.0.0-20210907225631-ff17edfbf26d // indirect
+	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83 // indirect
+)
