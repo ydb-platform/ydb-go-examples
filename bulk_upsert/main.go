788e11e8de73bda1ff0deeb7d182dae05d94d02d
asmyasnikov <79263256394@ya.ru>
2022-02-15 13:40:09 +0300

    update ydb-go-sdk to v3.10.0-rc1

--- bulk_upsert/main.go
+++ bulk_upsert/main.go
@@ -8,8 +8,10 @@ import (
 	"os"
 	"path"
 
-	environ "github.com/ydb-platform/ydb-go-sdk-auth-environ"
+	ydb "github.com/ydb-platform/ydb-go-sdk/v3"
 	"github.com/ydb-platform/ydb-go-sdk/v3/sugar"
+
+	environ "github.com/ydb-platform/ydb-go-sdk-auth-environ"
 )
 
 var (
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/ydb-platform/ydb-go-sdk-auth-environ v0.1.0
-	github.com/ydb-platform/ydb-go-sdk/v3 v3.9.0
+	github.com/ydb-platform/ydb-go-sdk/v3 v3.10.0-rc1
 	github.com/ydb-platform/ydb-go-yc v0.6.1
 )
