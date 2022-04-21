0a45a15d472d00a6a3ae412aa5835b0b5d0044e1
asmyasnikov <79263256394@ya.ru>
2022-04-22 00:24:02 +0300

    fix linter issues

--- auth/anonymous_credentials/main.go
+++ auth/anonymous_credentials/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ydb-platform/ydb-go-sdk/v3"
+	ydb "github.com/ydb-platform/ydb-go-sdk/v3"
 )
 
 var (
--- auth/environ/main.go
+++ auth/environ/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	environ "github.com/ydb-platform/ydb-go-sdk-auth-environ"
-	"github.com/ydb-platform/ydb-go-sdk/v3"
+	ydb "github.com/ydb-platform/ydb-go-sdk/v3"
 )
 
 func main() {
