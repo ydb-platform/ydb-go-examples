ea78c3ebd22be8e98f78b46efbb7e94bee5bca06
Timofey Koolin <timofey.koolin@gmail.com>
2022-07-21 17:23:45 +0300

    write examples

--- topic/read-examples/write-examples.go
+++ topic/read-examples/write-examples.go
@@ -2,6 +2,8 @@ package readexamples
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/ydb-platform/ydb-go-sdk/v3"
 	"github.com/ydb-platform/ydb-go-sdk/v3/topic/topicoptions"
@@ -16,8 +18,40 @@ func CreateWriter() *topicwriter.Writer {
 		ydb.WithAccessTokenCredentials("..."),
 	)
 
-	w, _ := db.Topic().StartWriter("/database/topic", "producer-id",
+	// Simple
+	w, _ := db.Topic().StartWriter("/database/topic", "producer-id")
+
+	// WithSpecificCodec
+	w, _ = db.Topic().StartWriter("/database/topic", "producer-id",
 		topicoptions.WithCodec(topictypes.CodecGzip),
 	)
-	return r
+
+	// WithConnectionCallback
+	w, _ = db.Topic().StartWriter("/database/topic", "producer-id",
+		topicoptions.WithMessageGroupID("asd"),
+	)
+
+	// Wait ack from server before return from write
+	w, _ = db.Topic().StartWriter("/database/topic", "producer-id",
+		topicoptions.WithSyncWrite(true),
+	)
+
+	return w
+}
+
+func SimpleWriteMessages(ctx context.Context, w *topicwriter.Writer) {
+	for i := 0; i < 100; i++ {
+		m := topicwriter.Message{Data: strings.NewReader(fmt.Sprint(i))}
+		_ = w.Write(ctx, m)
+	}
+}
+
+func BatchedWriteMessages(ctx context.Context, w *topicwriter.Writer) {
+	count := 100
+	messages := make([]topicwriter.Message, 0, count)
+	for i := 0; i < count; i++ {
+		m := topicwriter.Message{Data: strings.NewReader(fmt.Sprint(i))}
+		messages = append(messages, m)
+	}
+	_ = w.Write(ctx, messages...)
 }
