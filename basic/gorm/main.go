422ee9039f689b83eac1267c40744c3ea8af70c6
asmyasnikov <79263256394@ya.ru>
2023-01-18 17:42:51 +0300

    gorm example

---
+++ basic/gorm/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	ydb "github.com/ydb-platform/gorm-driver"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func initDB() (*gorm.DB, error) {
+	if dsn, has := os.LookupEnv("POSTGRES_CONNECTION_STRING"); has {
+		return gorm.Open(postgres.New(postgres.Config{
+			DSN:                  dsn,
+			PreferSimpleProtocol: true,
+		}))
+	}
+	if dsn, has := os.LookupEnv("SQLITE_CONNECTION_STRING"); has {
+		return gorm.Open(sqlite.Open(dsn))
+	}
+	if dsn, has := os.LookupEnv("YDB_CONNECTION_STRING"); has {
+		return gorm.Open(ydb.Open(dsn))
+	}
+	return nil, errors.New("cannot initialize DB")
+}
+
+const dateISO8601 = "2006-01-02"
+
+func date(date string) time.Time {
+	t, err := time.Parse(dateISO8601, date)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func main() {
+	// connect
+	db, err := initDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// prepare scheme and migrations
+	if err = db.AutoMigrate(
+		&Series{},
+		&Season{},
+		&Episode{},
+	); err != nil {
+		panic(err)
+	}
+
+	// upsert data
+	if err = db.Create(data).Error; err != nil {
+		panic(err)
+	}
+
+	// get all series
+	var series []Series
+	if err = db.Find(&series).Error; err != nil {
+		panic(err)
+	}
+	log.Println("all known series:")
+	for _, s := range series {
+		log.Printf(
+			"  > [%s]     %s (%s)\n",
+			s.ID, s.Title, s.ReleaseDate.Format("2006"),
+		)
+		seasons, err := s.GetSeasons(db)
+		if err != nil {
+			panic(err)
+		}
+		for _, ss := range seasons {
+			log.Printf(
+				"    > [%s]   %s\n",
+				ss.ID, ss.Title,
+			)
+			episodes, err := ss.GetEpisodes(db)
+			if err != nil {
+				panic(err)
+			}
+			for _, e := range episodes {
+				log.Printf(
+					"      > [%s] [%s] %s\n",
+					e.ID, e.AirDate.Format(dateISO8601), e.Title,
+				)
+			}
+		}
+	}
+}
---
+++ basic/gorm/models.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"gorm.io/gorm/clause"
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Series struct {
+	ID          string    `gorm:"column:series_id;primarykey;not null"`
+	Title       string    `gorm:"column:title;not null"`
+	Info        string    `gorm:"column:series_info"`
+	Comment     string    `gorm:"column:comment"`
+	ReleaseDate time.Time `gorm:"column:release_date;not null"`
+
+	Seasons []Season
+}
+
+func (s *Series) BeforeCreate(tx *gorm.DB) (err error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return err
+	}
+	s.ID = id.String()
+	for _, season := range s.Seasons {
+		season.SeriesID = s.ID
+	}
+	return
+}
+
+func (s *Series) GetSeasons(tx *gorm.DB) ([]Season, error) {
+	return s.Seasons, tx.Order("first_aired").Find(&s.Seasons, clause.Eq{
+		Column: "series_id",
+		Value:  s.ID,
+	}).Error
+}
+
+type Season struct {
+	ID         string    `gorm:"column:season_id;primarykey"`
+	SeriesID   string    `gorm:"column:series_id;index"`
+	Title      string    `gorm:"column:title;not null"`
+	FirstAired time.Time `gorm:"column:first_aired;not null"`
+	LastAired  time.Time `gorm:"column:last_aired;not null"`
+
+	Episodes []Episode
+}
+
+func (s *Season) BeforeCreate(tx *gorm.DB) (err error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return err
+	}
+	s.ID = id.String()
+	for _, episode := range s.Episodes {
+		episode.SeasonID = s.ID
+	}
+	return
+}
+
+func (s *Season) GetEpisodes(tx *gorm.DB) (episodes []Episode, _ error) {
+	return s.Episodes, tx.Order("air_date").Find(&s.Episodes, clause.Eq{
+		Column: "season_id",
+		Value:  s.ID,
+	}).Error
+}
+
+type Episode struct {
+	ID       string    `gorm:"column:episode_id;primarykey"`
+	SeasonID string    `gorm:"column:season_id;index;not null"`
+	Title    string    `gorm:"column:title;not null"`
+	AirDate  time.Time `gorm:"column:air_date;not null"`
+}
+
+func (e *Episode) BeforeCreate(tx *gorm.DB) (err error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return err
+	}
+	e.ID = id.String()
+	return
+}
